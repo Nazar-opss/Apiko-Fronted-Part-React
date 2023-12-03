71ca280a6b324b91be1f9a6967ec4dca08031a08
Nazar-opss <nazar4uk132@gmail.com>
2023-12-03 19:38:27 +0200

    complete practice55, as much as i understand the task )

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const  docsLink = 'https://uk.legacy.reactjs.org/',
       imgLink = 'https://base-prod.rspb-prod.magnolia-platform.com/.imaging/focalpoint/_WIDTH_x_HEIGHT_/dam/jcr:45987bb8-1050-403e-8bc8-154fe4adc6a2/1045838839-Species-puffin-group-on-rock%20(1).jpg',
       h1 = 'This test text for homework page',
-      h2 = 'Hello, New Stranger!',
       p = 'Here we have the list:',
       span1 = 'Here we have text and link!',
       span2 = 'And the picture!!!',
-      text = 'Just text here'
+      text = 'Just text here',
+      firstNameSara = 'Sara', 
+      lastNameConnor = 'Connor',
+      firstNameJonh = 'Jonh', 
+      lastNameDoe = 'Doe'
+
 
 
 function TextH1() {
-  return ( <h1>{h1}</h1> );
+  return ( <h1 style={{color: 'green'}}>{h1}</h1> );
 }
 
 function TextH2() {
-  return ( <h2>{h2}</h2> );
+  return ( <h2 style={{textDecoration: 'underline'}}>Welcome to website, {FormatUserName(firstNameSara, lastNameConnor)}</h2> );
+}
+
+function TextH3() {
+  return ( <h3 style={{opacity: '25%'}}>Welcome to website, {FormatUserName(firstNameJonh, lastNameDoe)}</h3> );
 }
 
 function Br() {
@@ -47,7 +56,7 @@ function LiTab() {
 }
 
 function Docs() {
-  return <a href={docsLink}>It is link to our docs</a>
+  return <a href={docsLink} className='docs_link'>It is link to our docs</a>
 }
 
 function Image() {
@@ -70,7 +79,9 @@ function Ul() {
   )
 }
 
-
+function FormatUserName(firstName, lastName) {
+  return firstName + lastName
+}
 
 export default function App() {
   return(
@@ -79,12 +90,13 @@ export default function App() {
       <Br />
       <TextH2 />
       <Br />
+      <TextH3 />
+      <Br />
       <P />
       <Ul />
     </div>
   )
 }
-
 
 root.render(
   <React.StrictMode>
@@ -96,6 +108,10 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// Practice: Create your first react app
+// Завдання 1
+// Використовуючи інструкцію бібліотеки create-react-app(https://create-react-app.dev/) встановити та запустити свій перший React-проект. Переглянути наявні файли та структуру додатку. Опісля видалити усі допоміжні файли із папки src/ - має залишитись лише index.js. Код даного файлу також підправити, видаливши непотрібні залежності. Як результат додаток має просто рендерити стрічку My Test React App.
 
 // Завдання 2
 // Використовуючи підготовлений проект, створити компонент App (в існуючому файлі index.js або в окремому, який імпортувати в існуючий). Даний компонент і має рендерити додаток як результат. В компоненті App переписати заданий HTML через React-елементи та React-компоненти. Усі статичні текстові дані винести у змінні і поміщати в React-компоненти через вирази (всередині фігурних дужок). Те ж саме і з усіма посиланнями.
@@ -119,3 +135,8 @@ root.render(
 //    </li>
 //  </ul>
 // </div>
+// Завдання 3
+// Описати функцію formatUserName - вона повинна приймати 2 агрументи - (firstName, lastName). Дана функція повинна повертати строку, яка містить наступний текст - Welcome to website, <firstName> <lastName>. В раніше описаному компоненті App у тегу h2 замінити текст ‘Hello, New Stranger’ даною функцією. Результатом має бути вітання ‘Welcome to website, Sara Connor’. Добавити в розмітку ще один тег h3, який використовуватиме ту ж функцію і міститеме привітання для John Doe.
+
+// Завдання 4
+// Створити додатковий файл із стилями *.css у підготовленому проекті. Описати стилі для React-компонентів та елементів уже опрацьованого компонента App (по власному бажанню і дизайнерським баченням). Дані стилі імпортувати в index.js, та застосувати до компонентів через атрибут className. Стилі для тегу h1 повинні бути заданні через інлайн-стилі, а саме за допомогою атрибута style.
