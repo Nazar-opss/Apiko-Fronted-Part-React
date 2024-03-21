176f6f82c81c7497b18c4f4bd9272c48a4c99aa8
Nazar Boyko <nazar4uk132@gmail.com>
2024-03-22 00:00:35 +0200

    Changes

--- Practice81/my-app/src/App.js
+++ Practice81/my-app/src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useState } from "react";
+
 
 function App() {
+  const [number, setNumber] = useState(0)
+
+  function handleClick() {
+    setNumber(number + 1)
+  }
   return (
     <div className="App">
       <header className="header">
-        <img src={logo} alt="logo" className="logo"></img>
+        <img src={logo} alt="logo" className="logo" onClick={()=> console.log('Dumb')}></img>
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
           <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
-          <TextField id="input-with-sx" label="With sx" variant="standard" />
+          <TextField id="input-with-sx" label="Search" variant="standard" color="warning" />
         </Box>
+      
+        <Box onClick={handleClick} className='cart'sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+          <h2>Cart</h2>
+          <ShoppingCartIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
+          <span className="cart_counter">{number}</span>
+        </Box>
+ 
       </header>
     </div>
   );
