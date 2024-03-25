2c3e7e3ecdb8bc0a1b8716464ea26594ccbba9c1
Nazar Boyko <nazar4uk132@gmail.com>
2024-03-25 23:35:02 +0200

    Add fetch for random cocktail and render it

--- Practice81/my-app/src/App.js
+++ Practice81/my-app/src/App.js
@@ -4,11 +4,12 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useState } from "react";
-import Main from "./pages/Main.js";
-import Catalog from "./pages/Catalog.js";
+import React, { useState } from "react";
+import Main from "./pages/Main.js"
+import Catalog from "./pages/Catalog.js"
 import Cocktail from "./pages/Cocktail.js"
 
+export const CocktailContext = React.createContext()
 
 function App() {
   const [number, setNumber] = useState(0)
@@ -17,12 +18,13 @@ function App() {
     setNumber(number + 1)
   }
   return (
+  <CocktailContext.Provider value={{handleClick}} >
     <div className="App">
       <header className="header">
         <img src={logo} alt="logo" className="logo" onClick={()=> console.log('Dumb')}></img>
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
           <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
-          <TextField id="input-with-sx" label="Search" variant="standard" color="warning" />
+          <TextField id="input-with-sx" label="Пошук" variant="standard" color="warning" />
         </Box>
       
         <Box onClick={handleClick} className='cart'sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
@@ -50,6 +52,7 @@ function App() {
 
       </footer>
     </div>
+  </CocktailContext.Provider>
   );
 }
 
