407281ee49e4a635545b31a1a8fa41c8717ca33b
Nazar Boyko <nazar4uk132@gmail.com>
2024-03-28 23:40:09 +0200

    changes

--- Practice81/my-app/src/pages/MainPage.js
+++ Practice81/my-app/src/pages/MainPage.js
@@ -32,22 +32,26 @@ const Order = (props) => {
   )
 }
 
+// fix re render
 
 const useFetchCocktails = () => {
     const [cocktail, setCocktail] = useState('')
     const context = useContext(CocktailContext)
     useEffect(() => {
-      const fetch = async () => {
+      const fetchData = async () => {
         try {
-          await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-            .then(async response => await response.json())
-            .then(data => setCocktail(data.drinks[0]))
-            .catch((error) => console.log(error));
+          // await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+          //   .then(async response => await response.json())
+          //   .then(data => setCocktail(data.drinks[0]))
+          //   .catch((error) => console.log(error));
+          const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+          const jsonData = await response.json();
+          setCocktail(jsonData.drinks[0]);
         } catch (error) {
           console.log("Error", error)
         }
       }
-      fetch()
+      fetchData()
   }, [context.number])
     return { cocktail }
   }
