840de60a3916a3aadb374bea904d3585fe64c617
Nazar Boyko <nazar4uk132@gmail.com>
2024-04-10 23:40:37 +0300

    Started footer, but still need to fix re-render

--- Practice81/my-app/src/App.js
+++ Practice81/my-app/src/App.js
@@ -1,19 +1,58 @@
-import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import logo from './logo.svg'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, memo, useReducer } from "react";
 import MainPage from "./pages/MainPage.js"
 import Catalog from "./pages/Catalog.js"
 import Cocktail from "./pages/Cocktail.js"
 
 export const CocktailContext = React.createContext()
 
+const Footer = () => {
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+
+  return (
+    <div className="footer">
+      {
+        alphabet.map(symbol => (
+          <>
+            <Link key={symbol} to={`/Catalog/${symbol}`} href="#">{symbol}
+            /
+            </Link>
+          </>
+            
+          )
+        )
+      }
+    </div>
+  )
+}
+
 function App() {
   const [numberCart, setNumber] = useState(0)
+  const navigate = useNavigate();
+ 
+    const navigateHome = () => {
+      navigate("/")
+    }
 
+  const initialState = {count: 0};
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return {count: state.count + 1};
+      case 'decrement':
+        return {count: state.count - 1};
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleClick = useCallback(() => {
     setNumber(numberCart => numberCart + 1)
@@ -23,10 +62,10 @@ function App() {
   // }
   
   return (
-  <CocktailContext.Provider value={{handleClick, numberCart}} >
+  <CocktailContext.Provider value={{handleClick, numberCart, setNumber, initialState, reducer, state, dispatch}} >
     <div className="App">
       <header className="header">
-        <img src={logo} alt="logo" className="logo" onClick={()=> console.log('Dumb')}></img>
+        <img src={logo} alt="logo" className="logo" onClick={navigateHome}></img>
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
           <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
           <TextField id="input-with-sx" label="Пошук" variant="standard" color="warning" />
@@ -35,7 +74,8 @@ function App() {
         <Box onClick={handleClick} className='cart'sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
           <h2>Cart</h2>
           <ShoppingCartIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }}/>
-          <span className="cart_counter">{numberCart}</span>
+          {/* <span className="cart_counter">{numberCart}</span> */}
+          <span className="cart_counter">{state.count}</span>
         </Box>
       </header>
       <main>
@@ -54,7 +94,7 @@ function App() {
         </div>
       </main>
       <footer>
-
+        <Footer/>
       </footer>
     </div>
   </CocktailContext.Provider>
--- Practice81/my-app/src/pages/MainPage.js
+++ Practice81/my-app/src/pages/MainPage.js
@@ -1,10 +1,10 @@
-import React, { useContext, useState, useCallback } from 'react'
+import React, { useContext, useState, useCallback, memo, useReducer, useMemo } from 'react'
 import { useEffect } from 'react';
 import { Button } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { CocktailContext } from '../App';
 
-const RandomCocktail = (props) => {
+const RandomCocktail = memo((props) => {
   return ( 
     <div className='random_cocktail'>
       <img src={props.img} alt={props.drinkName} className='random_cocktail_img'></img>
@@ -14,11 +14,11 @@ const RandomCocktail = (props) => {
       </div>
     </div>
   )
-}
+})
 
 // TODO: Find why page re fetching cocktail when adding to cart
 
-const Order = (props) => {
+const Order = memo((props) => {
   return (
     <Button onClick={props.handleClick} variant="contained" endIcon={<ShoppingCartIcon />} 
       sx={{
@@ -30,13 +30,14 @@ const Order = (props) => {
       Замовити
     </Button>
   )
-}
+})
 
-// fix re render need to try useMemo
+// fix re render
 
 const useFetchCocktails = () => {
     const [cocktail, setCocktail] = useState('')
     const context = useContext(CocktailContext)
+    
     useEffect(() => {
       const fetchData = async () => {
         try {
@@ -47,7 +48,6 @@ const useFetchCocktails = () => {
           const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
           const jsonData = await response.json();
           setCocktail(jsonData.drinks[0]);
-          
         } catch (error) {
           console.log("Error", error)
         }
@@ -59,38 +59,29 @@ const useFetchCocktails = () => {
 
 function MainPage () { 
   const context = useContext(CocktailContext)
-  
+  const [numberCart, setNumber] = useState(0)
   const { cocktail } = useFetchCocktails()
+  
 
-  const handleClick = useCallback(() => {
-    context.handleClick();
-  }, [context]);
-
-  // useEffect(() => {
-  //   // const fetch = async () => {
-  //   //   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-  //   //   const newData = await response.json();
-  //   //   setCocktail(newData);
-  //   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-  //     .then(response => response.json())
-  //     .then(data => setCocktail(data.drinks[0]))
-  //     .catch((error) => console.log(error));
-  //   console.log("foe")
-  //   // fetch()
-  // }, [context.number])
+  function handleClick() {
+      setNumber(numberCart => numberCart + 1)
+  }
+  
 console.log(cocktail)
 const { strDrinkThumb, strDrink } = cocktail
+return useMemo(() =>{
   return (
     <div className=''>
       <h2  >Для вибору коктейлю скористайтесь пошуком або фільтром</h2>
       <div className='random'>
-        <h2>Персональна рекомендація</h2>
+        <h2>Персональна рекомендація: {numberCart}</h2>
         <RandomCocktail img={strDrinkThumb} drinkName={strDrink} 
-        // order={<Order handleClick={()=> console.log('Out')} />}/>    
-        order={<Order handleClick={handleClick} />}/>    
+        order={<Order handleClick={() => context.dispatch({type: 'increment'})} />}/>    
+        {/* order={<Order handleClick={handleClick} />}/>     */}
       </div>
     </div>
   )
+}, [strDrink, strDrinkThumb, context, numberCart])
 }
 
 export default MainPage
